1262ede9b848e79f40b2201825e2a21be482da0d
ThongNguyenDT <nguyenduongtienthong@gmail.com>
2024-03-29 13:42:29 +0700

    ad receive send to mobile through websocket

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, Response
+from flask import Flask, Response, request, jsonify
 from flask_sock import Sock
 
 app = Flask(__name__)
 sock = Sock(app)
+
+wses = []
 
 
 @app.route('/')
@@ -10,12 +12,25 @@ def hello_world():  # put application's code here
     return Response('Hello World!')
 
 
+@app.route('/broadcast', methods=['POST'])
+def broadcast():
+    data = request.json['msg']
+    for ws in wses:
+        try:
+            ws.send(data)
+        except:
+            continue
+    return jsonify({'success': True})
+
+
 @sock.route('/getNews')
 def getNews(ws):
+    wses.append(ws)
     while True:
         data = ws.receive(30)
         if data:
             ws.send(data)
+            wses.remove(ws)
         else:
             break
 
